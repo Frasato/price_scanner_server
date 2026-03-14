21750a4045b3c9b575a64f430e6f9da3fcfb320d
Frasato <gabriel.frasato@hotmail.com>
2026-03-14 10:30:30 -0300

    clean (logs): remove system out from server

--- src/main/java/org/mege/config/Server.java
+++ src/main/java/org/mege/config/Server.java
@@ -9,11 +9,9 @@ import java.net.Socket;
 public class Server {
     public void start() throws IOException {
         ServerSocket server = new ServerSocket(6500);
-        System.out.println("Servidor aguardando conexões na porta 6500...");
 
         while (true) {
             Socket socket = server.accept();
-            System.out.println("Terminal conectado: " + socket.getInetAddress());
 
             Thread t = new Thread(new TerminalHandler(socket));
             t.start();
